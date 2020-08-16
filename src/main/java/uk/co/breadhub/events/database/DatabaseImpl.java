57fd9655b7f99588c1e3cd41586e8c4f55a02000
braddevans <braddevans123@gmail.com>
2020-08-16 01:18:07 +0100

    .-.

--- src/main/java/uk/co/breadhub/events/database/DatabaseImpl.java
+++ src/main/java/uk/co/breadhub/events/database/DatabaseImpl.java
@@ -131,10 +131,89 @@ public class DatabaseImpl {
 
 	public static void addPlayerStats(String playername, UUID uuid) {
 		try {
-			PreparedStatement ps = getConnection().prepareStatement("INSERT INTO `playerStatistics` (`UUID`, `PlayerName`) VALUES (?,?) ON DUPLICATE KEY UPDATE PlayerName=?;");
+			PreparedStatement ps = getConnection().prepareStatement("INSERT IGNORE INTO `playerStatistics` (" +
+					"`UUID`, " +
+					"`DAMAGE_DEALT`, " +
+					"`DAMAGE_TAKEN`, " +
+					"`DEATHS`, " +
+					"`MOB_KILLS`, " +
+					"`PLAYER_KILLS`, " +
+					"`FISH_CAUGHT`, " +
+					"`ANIMALS_BRED`, " +
+					"`LEAVE_GAME`, " +
+					"`JUMP`, " +
+					"`DROP_COUNT`, " +
+					"`DROP`, " +
+					"`PICKUP`, " +
+					"`PLAY_ONE_MINUTE`, " +
+					"`WALK_ONE_CM`, " +
+					"`WALK_ON_WATER_ONE_CM`, " +
+					"`FALL_ONE_CM`, " +
+					"`SNEAK_TIME`, " +
+					"`CLIMB_ONE_CM`, " +
+					"`FLY_ONE_CM`, " +
+					"`WALK_UNDER_WATER_ONE_CM`, " +
+					"`MINECART_ONE_CM`, " +
+					"`BOAT_ONE_CM`, " +
+					"`PIG_ONE_CM`, " +
+					"`HORSE_ONE_CM`, " +
+					"`SPRINT_ONE_CM`, " +
+					"`CROUCH_ONE_CM`, " +
+					"`AVIATE_ONE_CM`, " +
+					"`MINE_BLOCK`, " +
+					"`USE_ITEM`, " +
+					"`BREAK_ITEM`, " +
+					"`CRAFT_ITEM`, " +
+					"`KILL_ENTITY`, " +
+					"`ENTITY_KILLED_BY`, " +
+					"`TIME_SINCE_DEATH`, " +
+					"`TALKED_TO_VILLAGER`, " +
+					"`TRADED_WITH_VILLAGER`, " +
+					"`CAKE_SLICES_EATEN`, " +
+					"`CAULDRON_FILLED`, " +
+					"`CAULDRON_USED`, " +
+					"`ARMOR_CLEANED`, " +
+					"`BANNER_CLEANED`, " +
+					"`BREWINGSTAND_INTERACTION`, " +
+					"`BEACON_INTERACTION`, " +
+					"`DROPPER_INSPECTED`, " +
+					"`HOPPER_INSPECTED`, " +
+					"`DISPENSER_INSPECTED`, " +
+					"`NOTEBLOCK_PLAYED`, " +
+					"`NOTEBLOCK_TUNED`, " +
+					"`FLOWER_POTTED`, " +
+					"`TRAPPED_CHEST_TRIGGERED`, " +
+					"`ENDERCHEST_OPENED`, " +
+					"`ITEM_ENCHANTED`, " +
+					"`RECORD_PLAYED`, " +
+					"`FURNACE_INTERACTION`, " +
+					"`CRAFTING_TABLE_INTERACTION`, " +
+					"`CHEST_OPENED`, " +
+					"`SLEEP_IN_BED`, " +
+					"`SHULKER_BOX_OPENED`, " +
+					"`TIME_SINCE_REST`, " +
+					"`SWIM_ONE_CM`, " +
+					"`DAMAGE_DEALT_ABSORBED`, " +
+					"`DAMAGE_DEALT_RESISTED`, " +
+					"`DAMAGE_BLOCKED_BY_SHIELD`, " +
+					"`DAMAGE_ABSORBED`, " +
+					"`DAMAGE_RESISTED`, " +
+					"`CLEAN_SHULKER_BOX`, " +
+					"`OPEN_BARREL`, " +
+					"`INTERACT_WITH_BLAST_FURNACE`, " +
+					"`INTERACT_WITH_SMOKER`, " +
+					"`INTERACT_WITH_LECTERN`, " +
+					"`INTERACT_WITH_CAMPFIRE`, " +
+					"`INTERACT_WITH_CARTOGRAPHY_TABLE`, " +
+					"`INTERACT_WITH_LOOM`, " +
+					"`INTERACT_WITH_STONECUTTER`, " +
+					"`BELL_RING`, " +
+					"`RAID_TRIGGER`, " +
+					"`RAID_WIN`, " +
+					"`INTERACT_WITH_ANVIL`, " +
+					"`INTERACT_WITH_GRINDSTONE`) " +
+					"VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);");
 			ps.setString(1, uuid.toString());
-			ps.setString(2, playername);
-			ps.setString(3, playername);
 			ps.executeUpdate();
 		} catch(Exception e) {
 			e.printStackTrace();
@@ -142,9 +221,9 @@ public class DatabaseImpl {
 	}
 
 
-	public static String getPlayerFromUUID(UUID uuid) {
+	public static String getPlayerStatsByUuid(UUID uuid) {
 		try {
-			PreparedStatement ps = getConnection().prepareStatement("SELECT PlayerName FROM UUIDPlayerDB WHERE UUID=?");
+			PreparedStatement ps = getConnection().prepareStatement("SELECT * FROM playerStatistics WHERE UUID=?");
 			ps.setString(1, uuid.toString());
 			ResultSet rs = ps.executeQuery();
 			if(rs.next()) {
