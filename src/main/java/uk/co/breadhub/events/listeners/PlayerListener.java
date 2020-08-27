5363fb7f26cfb2d80007cbc924ba9ddb90d66384
braddevans <braddevans123@gmail.com>
2020-08-27 04:27:19 +0100

    remove players from scoreboard to save on memory

--- src/main/java/uk/co/breadhub/events/listeners/PlayerListener.java
+++ src/main/java/uk/co/breadhub/events/listeners/PlayerListener.java
@@ -4,6 +4,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import uk.co.breadhub.events.Main;
 import uk.co.breadhub.events.utils.MiscUtils;
 
 public class PlayerListener implements Listener {
@@ -12,5 +14,12 @@ public class PlayerListener implements Listener {
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
         MiscUtils.createScoreboardForPlayer(player);
+    }
+
+    @EventHandler
+    public void onPlayerDisconnect(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        // remove players from scoreboard to save on memory
+        Main.getInstance().boards.remove(player);
     }
 }
